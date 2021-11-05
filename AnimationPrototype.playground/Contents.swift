fd5a6c6ae629917b64befbe88e9ca0190cf1c217
Lucas Pereira <lucasgpereira.10@gmail.com>
2021-11-05 18:00:04 +0100

    Setting up main animation

--- AnimationPrototype.playground/Contents.swift
+++ AnimationPrototype.playground/Contents.swift
@@ -5,26 +5,53 @@ import PlaygroundSupport
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+    @State private var animationColor = Color.red
     
     var body: some View {
         ZStack {
             // Circle 1
             Circle()
                 .frame(width: 200, height: 200, alignment: .center)
-                .foregroundColor(.red)
+                .foregroundColor(.white)
+                .colorMultiply(animationColor)
+                .scaleEffect(animationAmount)
+                .animation(
+                    .easeInOut(duration: 2)
+                        .repeatForever(autoreverses: true),
+                    value: animationAmount
+                )
+                .onAppear {
+                    animationAmount = 1.61
+                    animationColor = Color.blue
+                }
             
             // Circle 2
             Circle()
                 .frame(width: 124, height: 124, alignment: .center)
                 .foregroundColor(.white)
                 .opacity(0.62)
+                .scaleEffect(animationAmount)
+                .animation(
+                    .easeInOut(duration: 1.8)
+                        .delay(0.2)
+                        .repeatForever(autoreverses: true),
+                    value: animationAmount
+                )
             
             // Circle 2
             Circle()
                 .frame(width: 77, height: 77, alignment: .center)
                 .foregroundColor(.white)
                 .opacity(0.62)
-        }.frame(width: 300, height: 450, alignment: .center)
+                .scaleEffect(animationAmount)
+                .animation(
+                    .easeInOut(duration: 1.6)
+                        .delay(0.4)
+                        .repeatForever(autoreverses: true),
+                    value: animationAmount
+                )
+            
+        }.frame(width: 400, height: 550, alignment: .center)
     }
 }
 
